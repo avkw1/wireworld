f862f48b97adc26403899ac6960536c03bb757d4
Alexander Korolev <avkw@novoenebo.com>
2023-10-31 14:54:37 +0300

    Добавлено масштабирование, удалены лишние пробелы
    Signed-off-by: Alexander Korolev <avkw@novoenebo.com>

--- wireworld.pas
+++ wireworld.pas
@@ -11,11 +11,10 @@ const
   CellSizeConst = 1;
   /// Имя файла
   wwFileName = 'ww800x600.gif';
-  /// Быстрый режим (эксперимент) 
+  /// Быстрый режим (эксперимент)
   fastMode = false;
   /// Количество шагов без перерисовки для быстрого режима
   fastModeSteps = 100;
-  
 
 type
   /// Состояние клетки (перечислимый тип)
@@ -25,7 +24,7 @@ type
     signal,     /// сигнал
     signal_tail /// "хвост" сигнала
   );
-  
+
   /// Клетка -------------------------------------------------------------------
   Cell = class
   public
@@ -37,7 +36,7 @@ type
     static signalColor: Color := clWhite;
     /// цвет хвоста сигнала
     static signalTailColor: Color := RGB(0, 128, 255); // ff0080ff
-  
+
   private
     /// состояние
     state: CellState;
@@ -49,14 +48,14 @@ type
     x: integer;
     /// вертикальная координата (для рисования)
     y: integer;
-  
+
   public
     /// вернуть состояние
     function getState: CellState;
     begin
       result := state
     end;
-    
+
     /// связать с соседями
     procedure setNeighbors(n1, n2, n3, n4, n5, n6, n7, n8: Cell);
     begin
@@ -69,14 +68,14 @@ type
       neighbors[7] := n7;
       neighbors[8] := n8;
     end;
-    
+
     /// установить координаты для рисования
     procedure setCoordinates(x, y: integer);
     begin
       self.x := x;
       self.y := y;
     end;
-    
+
     /// "инкремент" состояния
     procedure incState;
     begin
@@ -88,7 +87,7 @@ type
       end;
       newState := state;
     end;
-    
+
     /// "декремент" состояния
     procedure decState;
     begin
@@ -100,7 +99,7 @@ type
       end;
       newState := state;
     end;
-    
+
     /// нарисовать
     procedure draw(size: integer := 1);
     begin
@@ -122,7 +121,7 @@ type
         FillRectangle(x, y, x + size, y + size);
       end;
     end;
-    
+
     /// вычислить новое состояние
     procedure calcNewState;
     begin
@@ -144,47 +143,55 @@ type
         signal_tail: newState := wire;
       end;
     end;
-    
+
     /// состояние изменилось?
     function stateChanged: boolean;
     begin
       result := state <> newState
     end;
-    
+
     /// установить новое состояние
     procedure setNewState;
     begin
       state := newState
     end;
-    
+
     /// установить состояние
     procedure setState(cs: CellState);
     begin
       state := cs;
       newState := cs;
     end;
-    
+
     /// очистить сигналы
     procedure clearSignals;
     begin
       if (state = signal) or (state = signal_tail) then
         setState(wire);
     end;
-  
+
   end;
-  
+
   /// Игровое поле -------------------------------------------------------------
   GameField = class
+  public
+    /// цвет фона (вокруг поля)
+    static bgColor: Color := clGray;
+
   private
     /// клетки поля
     cells: array [1..N, 1..M] of Cell;
+    /// горизонтальная координата
+    x0: integer;
+    /// вертикальная координата
+    y0: integer;
     /// размер клетки
     cellSize: integer;
     /// номер поколения
     nGen: cardinal;
     /// флаг остановки
     stop: boolean := true;
-  
+
   public
     constructor Create(cellSize: integer := 1);
     begin
@@ -194,8 +201,7 @@ type
       for var i := 1 to N do
         for var j := 1 to M do
           cells[i, j] := new Cell;
-      // связывание с соседями, установка координат
-      var y := 0;
+      // связывание с соседями
       for var i := 1 to N do
       begin
         var i1 := i - 1;
@@ -204,7 +210,6 @@ type
         var i2 := i + 1;
         if i2 = N + 1 then
           i2 := 1;
-        var x := 0;
         for var j := 1 to M do
         begin
           var j1 := j - 1;
@@ -216,24 +221,24 @@ type
           cells[i, j].setNeighbors(
             cells[i1, j], cells[i1, j2], cells[i, j2], cells[i2, j2],
             cells[i2, j], cells[i2, j1], cells[i, j1], cells[i1, j1]);
-          cells[i, j].setCoordinates(x, y);
-          x += cellSize;
         end;
-        y += cellSize;
       end;
+      // установить координаты клеток, нарисовать
+      resize;
     end;
-    
+
     /// нарисовать
     procedure draw;
     begin
       LockDrawing;
       setWindowTitle;
+      clearWindow(bgColor);
       for var i := 1 to N do
         for var j := 1 to M do
           cells[i, j].draw(cellSize);
       UnlockDrawing;
     end;
-    
+
     /// перерисовать не пустые клетки
     procedure redrawNotEmpty;
     begin
@@ -245,7 +250,7 @@ type
             cells[i, j].draw(cellSize);
       UnlockDrawing;
     end;
-    
+
     /// переход к следующему шагу (без рисования!)
     procedure nextStep();
     begin
@@ -257,7 +262,7 @@ type
         for var j := 1 to M do
           cells[i, j].setNewState;
     end;
-    
+
     /// переход к следующему шагу и его отрисовка
     procedure nextStepAndDraw();
     begin
@@ -277,7 +282,7 @@ type
           end;
       UnlockDrawing;
     end;
-    
+
     /// запуск игры
     procedure play;
     begin
@@ -299,13 +304,13 @@ type
       else
         stop := true;
     end;
-    
+
     /// остановлено?
     function stopped: boolean;
     begin
       result := stop;
     end;
-    
+
     /// очистить (все клетки пустые)
     procedure clear;
     begin
@@ -315,7 +320,7 @@ type
           cells[i, j].setState(empty);
       draw;
     end;
-    
+
     /// очистить сигналы
     procedure clearSignals;
     begin
@@ -325,7 +330,7 @@ type
           cells[i, j].clearSignals;
       draw;
     end;
-    
+
     /// загрузить изображение
     procedure loadPicture(fname: string);
     begin
@@ -350,41 +355,78 @@ type
         draw;
       end;
     end;
-    
+
     /// установить заголовок окна
     procedure setWindowTitle;
     begin
       window.Title := 'Клеточный автомат WireWorld [Поколение ' + nGen + ']';
     end;
-    
+
+    /// увеличить масштаб
+    procedure scaleUp;
+    begin
+      if cellSize < 64 then
+        cellSize *= 2;
+      resize;
+    end;
+
+    /// уменьшить масштаб
+    procedure scaleDown;
+    begin
+      if cellSize > 1 then
+        cellSize := cellSize div 2;
+      resize;
+    end;
+
     /// обработчик мышки
     procedure mouseDown(x, y, mb: integer);
     begin
       if stopped then
       begin
-        var i := y div CellSize + 1;
-        var j := x div CellSize + 1;
-        case mb of 
+        var i := (y - y0) div CellSize + 1;
+        var j := (x - x0) div CellSize + 1;
+        case mb of
           1: cells[i, j].incState;
           2: cells[i, j].decState;
         end;
         cells[i, j].draw(CellSize);
       end;
     end;
-    
+
     /// обработчик клавиатуры
     procedure keyDown(k: integer);
     begin
+      case k of
+        VK_Space: play;
+        VK_PageUp: scaleUp;
+        VK_PageDown: scaleDown;
+      end;
       if stopped then
         case k of
           VK_Right: nextStepAndDraw;
           VK_Delete: clear;
           VK_Back: clearSignals;
-          VK_Space: play;
           VK_Home: loadPicture(wwFileName);
         end
-      else if k = VK_Space then
-        play;
+    end;
+
+    /// обработчик изменения размера окна
+    procedure resize;
+    begin
+      x0 := (window.Width - M * cellSize) div 2;
+      y0 := (window.Height - N * cellSize) div 2;
+      var y := y0;
+      for var i := 1 to N do
+      begin
+        var x := x0;
+        for var j := 1 to M do
+        begin
+          cells[i, j].setCoordinates(x, y);
+          x += cellSize;
+        end;
+        y += cellSize;
+      end;
+      draw;
     end;
   end;
 
@@ -404,13 +446,20 @@ begin
   game.keyDown(k);
 end;
 
+// Обработчик изменения размера окна
+procedure resize;
+begin
+  game.resize;
+end;
+
 // Основная процедура
 begin
+  SetSmoothingOff;
   window.SetSize(M * CellSizeConst, N * CellSizeConst);
-  window.IsFixedSize := true;
   window.CenterOnScreen;
   game := new GameField(CellSizeConst);
   game.loadPicture(wwFileName);
   OnMouseDown := mouseDown;
   OnKeyDown := keyDown;
+  OnResize := resize;
 end.
